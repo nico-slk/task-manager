daaa17d31b64eecb89fc9ceff7eb91cebd3d813d
nico1991.6 <nico1991.6@gmail.com>
2024-12-28 18:25:40 +0000

    db

--- src/connection/db.ts
+++ src/connection/db.ts
@@ -1,8 +1,11 @@
 import * as mysql2 from 'mysql2';
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } = process.env;
-const dbURL = `mysql://${DB_USER}:${DB_PASSWORD}@junction.proxy.rlwy.net:33074/${DB_NAME}`;
+const dbURL = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const db = new Sequelize(dbURL, {
     host: DB_HOST,
