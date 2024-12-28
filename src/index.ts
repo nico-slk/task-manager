045377a8a43eaf299b38ac46f0123490409ebe12
nico1991.6 <nico1991.6@gmail.com>
2024-12-28 16:19:36 +0000

    config first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT || '3000');
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
