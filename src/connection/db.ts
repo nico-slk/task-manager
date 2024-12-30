3874e2e2ce46ce8152c75b7cfe5a91e93dba9ea3
nico-slk <nicolas.selicki@gmail.com>
2024-12-30 15:53:44 -0300

    db config railway

--- src/connection/db.ts
+++ src/connection/db.ts
@@ -3,10 +3,19 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-const { DB_NAME, DB_HOST, DB_PORT, MONGO_HOST, MONGO_PORT, MONGO_NAME } =
-  process.env;
+const {
+  MONGO_PASSWORD,
+  MONGO_USER,
+  DB_NAME,
+  MONGO_HOST,
+  MONGO_PORT,
+  MONGO_NAME,
+} = process.env;
 
-const dbURL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const dbURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}`;
+
+console.log(dbURL);
+
 // const dbURL = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_NAME}`;
 
 export const connectDB = async () => {
