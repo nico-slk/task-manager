6e9a4fb9be6e649a2b436b3e5ff4b8d5fc34dc84
nico-slk <nicolas.selicki@gmail.com>
2024-12-30 15:43:45 -0300

    done

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,20 +1,20 @@
-import { Router } from 'express'
-import { body, param } from 'express-validator'
-import taskService from '../services/task.service'
-import { ValidJWT, ValidateRequest } from '../utils'
+import { Router } from 'express';
+import { body, param } from 'express-validator';
+import taskService from '../services/task.service';
+import { ValidJWT, ValidateRequest } from '../utils';
 
-const { validJwt } = ValidJWT
-const { validateRequest } = ValidateRequest
+const { validJwt } = ValidJWT;
+const { validateRequest } = ValidateRequest;
 
 const { createTask, deleteTask, getAllTasks, getTaskById, updateTask } =
-  taskService
+  taskService;
 
-const router = Router()
+const router = Router();
 
 // Validaciones
 const idValidation = param('id')
   .isMongoId()
-  .withMessage('El ID debe ser un MongoID válido')
+  .withMessage('El ID debe ser un MongoID válido');
 const taskValidation = [
   body('title')
     .isString()
@@ -29,19 +29,19 @@ const taskValidation = [
     .withMessage(
       'El estado debe ser uno de los siguientes: pending, in-progress, completed',
     ),
-]
+];
 
 // Rutas
-router.get('/', getAllTasks)
-router.get('/:id', idValidation, validJwt, validateRequest, getTaskById)
-router.post('/', taskValidation, validJwt, validateRequest, createTask)
+router.get('/', [validJwt], getAllTasks);
+router.get('/:id', idValidation, validJwt, validateRequest, getTaskById);
+router.post('/', taskValidation, validJwt, validateRequest, createTask);
 router.put(
   '/:id',
   [idValidation, ...taskValidation],
   validJwt,
   validateRequest,
   updateTask,
-)
-router.delete('/:id', idValidation, validJwt, validateRequest, deleteTask)
+);
+router.delete('/:id', idValidation, validJwt, validateRequest, deleteTask);
 
-export default router
+export default router;
